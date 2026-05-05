8babfd908044b0a18066d6ac83e0ab86c30d6de3
Niranjan Vishwakarma <niranjan4391@gmail.com>
2026-05-05 12:50:44 +0530

    updated aboutus info

--- website/context_processors.py
+++ website/context_processors.py
@@ -4,7 +4,7 @@ def site_settings(request):
         'site_subtitle': 'Jharkhand State Centre, Ranchi',
         'site_phone': '06513510412',
         'site_email': 'jharkhandsc@ieindia.org',
-        'site_address': 'IEI Building, Shyamali, Doranda, Ranchi - 834002, Jharkhand',
+        'site_address': 'Engineer''s Bhavan, Nepal Kothi Campus, Ranchi - 834002, Jharkhand',
         'whatsapp_number': '919430161766',
         'facebook_url': 'https://facebook.com',
         'twitter_url': 'https://twitter.com',
